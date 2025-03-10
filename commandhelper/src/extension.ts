271126da4b2a821eb79cb06ba29ba23b15e4c1d5
Natmax93 <natmax93@natmax-omen.home>
2025-03-10 15:46:43 +0100

    Règles tab et ctrl+A

--- commandhelper/src/extension.ts
+++ commandhelper/src/extension.ts
@@ -1,63 +1,112 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fs from 'fs';
+import { spawn } from 'child_process';
+import { json } from 'stream/consumers';
+
+// Représentation de la position d'un curseur sous forme de tuple (ligne, colonne)
+type Position = [number, number];
+
+// Représentation de l'état d'un curseur par sa position courante, la position du
+// début de la sélection et la position de la fin de la sélection
+type CursorState = {
+	position:Position,
+	start:Position,
+	end:Position
+};
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	const editor = vscode.window.activeTextEditor;
+
+	// On initialise la liste des états
+	let liste_etats_texte: string[] = [];
+	let liste_etats_curseur: CursorState[] = [];
+	let etats = {
+		"etats_texte": liste_etats_texte,
+		"etats_curseur": liste_etats_curseur
+	};
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commandhelper" is now active!');
-
-	vscode.window.onDidChangeTextEditorSelection((event: vscode.TextEditorSelectionChangeEvent) => {
-		// Event kind : The change kind which has triggered this event. Can be undefined.
-		// 1: Keyboard
-		// 2: Mouse
-		// 3: Command
-		console.log(`Event kind : ${event.kind}`)
-
-		// Event selection : The new value for the text editor's selections.
-		// active : position of the cursor
-		// anchor : position at which the selection starts
-		// start : start position. It is before or equal to end.
-		// end : end position. It is after or equal to start.
-		console.log(`Event selection active (character:number, line:number): ${event.selections[0].active.character}, ${event.selections[0].active.line}`)
-		console.log(`Event selection anchor (character:number, line:number): ${event.selections[0].anchor.character}, ${event.selections[0].active.line}`)
-
-		// Event textEditor : The text editor for which the selections have changed.
-		// Affiche le texte sélectionné
-		var rangeOfSelection = new vscode.Range(event.selections[0].start, event.selections[0].end)
-		console.log(`Event textEditor : ${event.textEditor.document.getText(rangeOfSelection)}`)
-
-
-	}, null, context.subscriptions);
+	console.log('Congratulations, your extension "command-helper" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commandhelper.readActiveFile', () => {
-		const editor = vscode.window.activeTextEditor;
-
-		if (editor) {
-			// Get the content of the currently acive file
-			const document = editor.document;
-
-			// Get the text from the entire document 
-			const fileContent = document.getText();
-
-			// Show file content in a message box (just for demo)
-			vscode.window.showInformationMessage('File Content:', fileContent.substring(0, 100)); // Just shows first 100 characters
-        } else {
-            vscode.window.showInformationMessage('No active editor found!');
-		}
-
+	const disposable = vscode.commands.registerCommand('command-helper.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from commandHelper!');
+		vscode.window.showInformationMessage('Hello World from Command Helper!');
 	});
 
-	context.subscriptions.push(disposable);
+	// Événement déclenché quand l'utilisateur modifie le texte
+    // let changeListener = vscode.workspace.onDidChangeTextDocument(event => {
+    const changeListener = vscode.workspace.onDidChangeTextDocument(event => {
+        if (editor && event.document === editor.document) {
+			const document = editor.document;
+			const text = document.getText();
+			
+			// On ajoute le nouvel état
+			liste_etats_texte.push(text);
+			const jsonString = JSON.stringify(etats);
+
+			// Exécution du script Python pour la recommandation
+			const pythonProcess = spawn('python', ['src/recommendation.py', jsonString]);
+
+			pythonProcess.stdout.on('data', (data) => {
+				vscode.window.showInformationMessage(`Commande recommandée : ${data}`);
+			});
+
+			pythonProcess.stderr.on('data', (data) => {
+				console.error(`(TypeScript) Erreur Python : ${data}`);
+			});
+        }
+    });
+
+	// Évènement qui se déclenche lorsque l'utilisateur déplace la souris ou change la sélection
+	const selectionListener = vscode.window.onDidChangeTextEditorSelection(event => {
+        if (event.textEditor === vscode.window.activeTextEditor) {
+
+			// Position actuelle du curseur
+            const position = event.selections[0].active;
+
+			// Position du début de la sélection
+			const start = event.selections[0].start;
+
+			// Position de la fin de la sélection
+			const end = event.selections[0].end;
+
+			liste_etats_curseur.push({
+				position: [position.line, position.character],
+				start: [start.line, start.character],
+				end: [end.line, end.character]
+			});
+
+			const jsonString = JSON.stringify(etats);
+
+			// Exécution du script Python pour la recommandation
+			const pythonProcess = spawn('python', ['src/recommendation.py', jsonString]);
+
+			pythonProcess.stdout.on('data', (data) => {
+				vscode.window.showInformationMessage(`Commande recommandée : ${data}`);
+				console.log(`Sortie Python : ${data}`);
+			});
+
+			pythonProcess.stderr.on('data', (data) => {
+				console.error(`(TypeScript) Erreur Python : ${data}`);
+			});
+        }
+    });
+
+	context.subscriptions.push(disposable, changeListener, selectionListener);
+}
+
+export function commentLine() {
+	vscode.commands.executeCommand('editor.action.addCommentLine');
 }
 
 // This method is called when your extension is deactivated
